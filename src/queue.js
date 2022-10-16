d878e28d9122efd1caba2a7a43226b2f5ea3a276
Aliaksei Rudak <lexarudak@gmail.com>
2022-10-16 16:14:50 +0600

    feat: solve queue task

--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+class ListNode {
+  constructor (value) {
+    this.value = value,
+    this.next = null
+  }
+}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Queue {
+  constructor () {
+    this.head = null
+    this.length = 0
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    console.log(this.head);
+    return this.head
+  }
+
+  enqueue(value) {
+    if (this.head === null) {
+      this.head = new ListNode (value)
+      this.length++
+    } else {
+    let current = this.head
+
+      while (current.next) {
+          current = current.next
+      }
+      current.next = new ListNode (value)
+      this.length++
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+   if (this.head !== null) {
+     const toRet = this.head.value
+     this.head.value = this.head.next.value
+     this.head.next = this.head.next.next
+     this.length--
+
+     return toRet
+   } else {
+     return false
+   }
+  } 
 }
 
 module.exports = {
   Queue
 };
+
+// const queue = new Queue()
+//     queue.enqueue(5);
+//     queue.enqueue(6);
+//     queue.enqueue(7);
+//     queue.dequeue()
+//     queue.dequeue()
